8400a112024983b4e89913c64260997680375886
Randolph Ledesma <randolphl@zylun.com>
2017-04-21 04:21:04 +0800

    some changes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
+import Hello from "./Hello.jsx";
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div style={{ textAlign: "center" }}>
-                <h1>React App</h1>
-            </div>
-        );
-    }
+  render() {
+    return <div><Hello name="Your Name Here" age="1" /></div>;
+  }
 }
---
+++ client/components/Hello.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Hello = ({ name, age }) => <div>{`Hi ${name} ${age}`}</div>;
+
+export default Hello;
